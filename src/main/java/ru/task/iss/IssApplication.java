946ac14308aaa2667cc4647fa66cda0ca389473a
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-04-20 13:18:22 +0500

    Init commit

---
+++ src/main/java/ru/task/iss/IssApplication.java
@@ -0,0 +1,13 @@
+package ru.task.iss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IssApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/task/iss/IssApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.task.iss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
