2b9fd236c7d5b38e1b8b2c0bbd4eb8f7a16e9502
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-04-22 12:28:43 +0500

    Add updateSecurity test method
    I've added some comments

--- src/test/java/ru/task/iss/repository/SecurityRepositoryTest.java
+++ src/test/java/ru/task/iss/repository/SecurityRepositoryTest.java
@@ -16,12 +16,14 @@ public class SecurityRepositoryTest extends AbstractRepositoryClass {
     @Autowired
     SecurityRepository repository;
 
+    /* Find all Securities */
     @Test
     public void findAll_ReturnsAllSecurities() {
         int quantity = repository.findAll().size();
         assertEquals(3, quantity);
     }
 
+    /* Find the Security by id*/
     @Test
     public void findById_ReturnsSecurityById() {
         Security security = repository.findById(12441).orElse(null);
@@ -29,6 +31,7 @@ public class SecurityRepositoryTest extends AbstractRepositoryClass {
         assertEquals("АбрауДюрсо", security.getShortname());
     }
 
+    /* Find the Security by secId */
     @Test
     public void findBySecId_ReturnsSecurityBySecIdPk() {
         Security security = repository.findBySecId("1").orElse(null);
@@ -36,6 +39,7 @@ public class SecurityRepositoryTest extends AbstractRepositoryClass {
         assertEquals("Apple Inc.", security.getName());
     }
 
+    /* Save a new Security entity */
     @Test
     public void save_ShouldSaveNewSecurity() {
         int before = repository.findAll().size();
@@ -46,9 +50,9 @@ public class SecurityRepositoryTest extends AbstractRepositoryClass {
         assertEquals(before + 1, after);
     }
 
-    // delete
+    /* Delete the Security by secId */
     @Test
-    public void deleteById_ShouldDeleteRecordById() {
+    public void deleteById_ShouldDeleteRecordBySecId() {
         int before = repository.findAll().size();
         repository.deleteBySecId("3");
         entityManager.flush();
@@ -57,5 +61,18 @@ public class SecurityRepositoryTest extends AbstractRepositoryClass {
         assertEquals(before - 1, after);
     }
 
-    // update
+    /* Update the Security shortname */
+    @Test
+    public void update_ShouldUpdateSecurityBySecId() {
+        Security security = repository.findBySecId("1").orElse(null);
+        assertNotNull(security);
+
+        assertEquals("Apple", security.getShortname());
+        security.setShortname("Pineapple");
+        entityManager.persistAndFlush(security);
+
+        Security updatedSecurity = repository.findBySecId("1").orElse(null);
+        assertNotNull(updatedSecurity);
+        assertEquals("Pineapple", updatedSecurity.getShortname());
+    }
 }
