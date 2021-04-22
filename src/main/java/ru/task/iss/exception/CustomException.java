fed40021c8288ac35bfaee1801fd7cadf5d05169
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-04-22 11:27:10 +0500

    Add CustomException

---
+++ src/main/java/ru/task/iss/exception/CustomException.java
@@ -0,0 +1,43 @@
+package ru.task.iss.exception;
+/*
+ * Date: 4/21/21
+ * Time: 3:32 PM
+ * */
+
+import org.springframework.http.HttpStatus;
+
+public final class CustomException extends RuntimeException {
+
+    private String message;
+    private HttpStatus httpStatus;
+
+    public CustomException(String message) {
+        super(message);
+    }
+
+    public CustomException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CustomException(String message, HttpStatus httpStatus) {
+        this.message = message;
+        this.httpStatus = httpStatus;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    public void setHttpStatus(HttpStatus httpStatus) {
+        this.httpStatus = httpStatus;
+    }
+}
