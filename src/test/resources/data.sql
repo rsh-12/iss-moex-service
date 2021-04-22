5c4e57da6af33d2ac0b50a0eb0208c8c0576e8d6
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-04-22 11:19:45 +0500

    Add test init dml script

---
+++ src/test/resources/data.sql
@@ -0,0 +1,18 @@
+insert into securities (id, sec_id, shortname, regnumber, name, isin, is_traded, emitent_id, emitent_title, emitent_inn,
+                        emitent_okpo, gosreg, type, "group", primary_boardid, marketprice_boardid)
+values (154676, '1', 'Apple', '',
+        'Apple Inc.', 'US0378331005', 1, 1281003,
+        'Apple Inc', '', '', '',
+        'common_share', 'stock_shares', 'EQRD', ''),
+
+       (12441, '2', 'АбрауДюрсо', '1-02-12500-A',
+        'Абрау-Дюрсо ПАО ао', 'RU000A0JS5T7', 1, 2556,
+        'Публичное акционерное общество "Абрау – Дюрсо"', 7727620673, '', '1 - 02 - 12500 - A',
+        'common_share', 'stock_shares', 'TQBR', 'TQBR'),
+
+       (131568, '3', 'АСКО ао', '1-01-52065-Z',
+        'АСКО-СТРАХОВАНИЕ ПАО ао', 'RU000A0JXS91', 1, 1098705,
+        'Публичное акционерное общество "АСКО-СТРАХОВАНИЕ"', 7453297458, '', '1-01-52065-Z',
+        'common_share', 'stock_shares', 'TQDE', 'TQDE');
+
+
