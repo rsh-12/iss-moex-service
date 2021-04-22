6cadcb92bdd581d59671d251f7b7e19f57d552d5
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-04-22 12:11:18 +0500

    Add createSecurity method

--- src/test/java/ru/task/iss/repository/AbstractRepositoryClass.java
+++ src/test/java/ru/task/iss/repository/AbstractRepositoryClass.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
+import ru.task.iss.entity.Security;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -16,4 +17,24 @@ public class AbstractRepositoryClass {
 
     @Autowired
     TestEntityManager entityManager;
+
+    Security createSecurity() {
+        Security security = new Security();
+        security.setId(1);
+        security.setSecId("123");
+        security.setShortname("short");
+        security.setRegnumber("");
+        security.setName("name");
+        security.setIsin("ISIN2323");
+        security.setIsTraded(12);
+        security.setEmitentId(212);
+        security.setEmitentTitle("emtitentTitle");
+        security.setEmitentOkpo("okpo");
+        security.setGosreg("");
+        security.setType("common-share");
+        security.setGroup("group");
+        security.setPrimaryBoardid("121");
+        security.setMarketPriceBoardid("mp12");
+        return security;
+    }
 }
