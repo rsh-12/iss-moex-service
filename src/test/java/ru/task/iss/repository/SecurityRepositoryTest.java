fb320057aa939161d424eac368b9f0a4617df181
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-04-22 12:22:50 +0500

    Add deleteBySecId test method

--- src/test/java/ru/task/iss/repository/SecurityRepositoryTest.java
+++ src/test/java/ru/task/iss/repository/SecurityRepositoryTest.java
@@ -40,12 +40,22 @@ public class SecurityRepositoryTest extends AbstractRepositoryClass {
     public void save_ShouldSaveNewSecurity() {
         int before = repository.findAll().size();
         Security security = createSecurity();
-        entityManager.persist(security);
+        entityManager.persistAndFlush(security);
         int after = repository.findAll().size();
 
         assertEquals(before + 1, after);
     }
 
     // delete
+    @Test
+    public void deleteById_ShouldDeleteRecordById() {
+        int before = repository.findAll().size();
+        repository.deleteBySecId("3");
+        entityManager.flush();
+
+        int after = repository.findAll().size();
+        assertEquals(before - 1, after);
+    }
+
     // update
 }
