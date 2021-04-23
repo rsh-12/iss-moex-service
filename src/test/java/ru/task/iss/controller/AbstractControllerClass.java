b531dab096cfd9c566d6994810d746c0e428a6d2
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-04-23 18:37:06 +0500

    Update AbstractControllerClass

--- src/test/java/ru/task/iss/controller/AbstractControllerClass.java
+++ src/test/java/ru/task/iss/controller/AbstractControllerClass.java
@@ -4,5 +4,21 @@ package ru.task.iss.controller;
  * Time: 5:50 PM
  * */
 
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@SpringBootTest
 public abstract class AbstractControllerClass {
+
+    final static String SECURITIES = "/api/securities/";
+
+    @Autowired
+    MockMvc mvc;
+
 }
