208915650719b8d19398856cbbf9d0ffa49b97a2
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-04-23 19:53:29 +0500

    Test it out security updating

--- src/test/java/ru/task/iss/controller/SecurityControllerTest.java
+++ src/test/java/ru/task/iss/controller/SecurityControllerTest.java
@@ -5,11 +5,12 @@ package ru.task.iss.controller;
  * */
 
 import org.junit.Test;
+import org.springframework.http.MediaType;
+import ru.task.iss.dto.SecurityDto;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.containsStringIgnoringCase;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -19,7 +20,7 @@ public class SecurityControllerTest extends AbstractControllerClass {
 
     /* Get the security by id - returns ok */
     @Test
-    public void findOne_ReturnsTheSecurityById() throws Exception {
+    public void findOne_ShouldReturnSecurityById() throws Exception {
         mvc.perform(get(SECURITIES + 154676))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("shortname", containsString("Apple")))
@@ -28,11 +29,36 @@ public class SecurityControllerTest extends AbstractControllerClass {
 
     /* Delete the security by id - returns 404 */
     @Test
-    public void deleteOne_ReturnsBadRequest() throws Exception {
+    public void deleteOne_ShouldDeleteSecurityById() throws Exception {
         mvc.perform(delete(SECURITIES + 999))
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("message",
                         containsStringIgnoringCase("security not found: " + 999)))
+                .andDo(print());
+    }
+
+    /* Update the security by id */
+    @Test
+    public void update_ShouldUpdateSecurity() throws Exception {
+        final int SECURITY_ID = 154676;
+
+        SecurityDto securityDto = new SecurityDto();
+        securityDto.setShortname("shortname");
+
+        mvc.perform(get(SECURITIES + SECURITY_ID))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("shortname", containsString("Apple")))
+                .andDo(print());
+
+        mvc.perform(patch(SECURITIES + SECURITY_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(convertToJson(securityDto)))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        mvc.perform(get(SECURITIES + SECURITY_ID))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("shortname", containsString("shortname")))
                 .andDo(print());
     }
 }
