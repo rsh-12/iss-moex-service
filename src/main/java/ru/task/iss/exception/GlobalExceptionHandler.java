25634719cec9e26f8aec5eb01ec67faa7b4c240f
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-04-23 15:44:49 +0500

    Fix GlobalExceptionHandler
    Have deleted MethodArgumentNotValidException exception handler, added
    ControllerAdvice annotation, uncommented another MANVE exception handler

--- src/main/java/ru/task/iss/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/task/iss/exception/GlobalExceptionHandler.java
@@ -4,40 +4,41 @@ package ru.task.iss.exception;
  * Time: 3:35 PM
  * */
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+@ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("error", ex);
-        return new ResponseEntity<>(body, HttpStatus.UNPROCESSABLE_ENTITY);
-    }
 
-//    @Override
-//    protected ResponseEntity<Object> handleMethodArgumentNotValid(
-//            MethodArgumentNotValidException ex,
-//            HttpHeaders headers,
-//            HttpStatus status, WebRequest request) {
-//
-//        final List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
-//
-//        Map<String, Set<String>> errorsMap = fieldErrors.stream().collect(
-//                Collectors.groupingBy(FieldError::getField,
-//                        Collectors.mapping(FieldError::getDefaultMessage, Collectors.toSet()))
-//        );
-//
-//        return new ResponseEntity<>(errorsMap.isEmpty() ? ex : errorsMap, headers, status);
-//    }
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatus status, WebRequest request) {
+
+        final List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+
+        Map<String, Set<String>> errorsMap = fieldErrors.stream().collect(
+                Collectors.groupingBy(FieldError::getField,
+                        Collectors.mapping(FieldError::getDefaultMessage, Collectors.toSet()))
+        );
+
+        return new ResponseEntity<>(errorsMap.isEmpty() ? ex : errorsMap, headers, status);
+    }
 
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleException(CustomException ex) {
