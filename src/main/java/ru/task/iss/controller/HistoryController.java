9a61de8bb9c2247e453df71b6431e10e0c65efb3
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-04-24 09:26:10 +0500

    Add HistoryController class

---
+++ src/main/java/ru/task/iss/controller/HistoryController.java
@@ -0,0 +1,18 @@
+package ru.task.iss.controller;
+/*
+ * Date: 4/24/21
+ * Time: 9:10 AM
+ * */
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/histories")
+public class HistoryController {
+
+    private static final Logger log = LoggerFactory.getLogger(HistoryController.class);
+
+}
