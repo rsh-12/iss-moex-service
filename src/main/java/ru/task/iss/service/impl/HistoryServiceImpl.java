44fa74b921c866691e610da2a94abd0144ef9508
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-04-24 15:55:45 +0500

    Add some comments and implement findAllHistories

--- src/main/java/ru/task/iss/service/impl/HistoryServiceImpl.java
+++ src/main/java/ru/task/iss/service/impl/HistoryServiceImpl.java
@@ -5,6 +5,7 @@ package ru.task.iss.service.impl;
  * */
 
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,12 +16,11 @@ import ru.task.iss.repository.HistoryRepository;
 import ru.task.iss.service.HistoryService;
 import ru.task.iss.util.HistoryXmlParser;
 
-import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
 
 @Service
-public class HistoryServiceImpl implements HistoryService {
+public class HistoryServiceImpl extends AbstractServiceClass implements HistoryService {
 
     private final HistoryRepository historyRepository;
     private final ModelMapper mapper;
@@ -30,29 +30,69 @@ public class HistoryServiceImpl implements HistoryService {
         this.mapper = mapper;
     }
 
+    /**
+     * Converts MultipartFile to a File.
+     * Parses and initializes the entity, then saves to the DB.
+     *
+     * @param file XML Multipart file.
+     * @throws CustomException if file is not found, catches an IOException,
+     *                         a ParserConfigurationException or a SAXException.
+     */
     @Override
-    public void importXmlData(MultipartFile file) throws IOException {
+    public void importXmlData(MultipartFile file) {
         HistoryXmlParser historyXmlParser = new HistoryXmlParser(historyRepository);
         historyXmlParser.parseAndSave(file);
     }
 
+    /**
+     * Creates a new History object and saves to the DB.
+     *
+     * @param historyDto DTO for History object.
+     */
     @Override
     public void create(HistoryDto historyDto) {
         History history = mapper.map(historyDto, History.class);
         save(history);
     }
 
+    /**
+     * Method searches for records in the DB and always returns a list of History objects or an empty list
+     *
+     * @param pageNo    page number - default 0.
+     * @param pageSize  page size - default 10 elements.
+     * @param sort      sorting - default by secId.
+     * @param tradeDate filtering.
+     * @return list of Hisotry objects.
+     */
     @Override
-    public List<History> findAllHistories(Integer pageNo, Integer pageSize, String sort, LocalDate tradeDate) {
-        return null;
+    public List<History> findAllHistories(Integer pageNo, Integer pageSize,
+                                          String sort, LocalDate tradeDate) {
+        PageRequest pageRequest = getPageRequest(pageNo, pageSize, sort, HISTORY_FIELDS);
+
+        if (tradeDate == null) {
+            return historyRepository.findAll(pageRequest).getContent();
+        }
+        return historyRepository.findAll(pageRequest, tradeDate).getContent();
     }
 
+    /**
+     * Method searches for a specific record by ID in the DB.
+     *
+     * @param id History object identifier.
+     * @return a History object.
+     * @throws CustomException if the record is not found in the DB.
+     */
     @Override
     public History findById(Long id) {
         return historyRepository.findById(id).orElseThrow(() ->
                 new CustomException("Not Found", "History not found: " + id, HttpStatus.NOT_FOUND));
     }
 
+    /**
+     * Method deletes a record by ID from the DB.
+     * @param id a History object identifier.
+     * @throws CustomException if the record is not found in the DB.
+     */
     @Override
     public void deleteById(Long id) {
         if (!historyRepository.existsById(id)) {
@@ -61,6 +101,12 @@ public class HistoryServiceImpl implements HistoryService {
         historyRepository.deleteById(id);
     }
 
+    /**
+     * Updates a record fields and then saves to the DB.
+     *
+     * @param id a History object identifier.
+     * @param historyDto DTO for History entity.
+     */
     @Override
     public void update(Long id, HistoryDto historyDto) {
         History history = historyRepository.findById(id).orElseThrow(() ->
@@ -70,6 +116,11 @@ public class HistoryServiceImpl implements HistoryService {
         save(history);
     }
 
+    /**
+     * Saves a History to the DB.
+     *
+     * @param history is a History object.
+     */
     public void save(History history) {
         historyRepository.save(history);
     }
