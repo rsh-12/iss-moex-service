907b02dca3c66c896b68d8d753ffeff7b7c94554
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-04-24 13:19:02 +0500

    Add HistoryXmlParser class

---
+++ src/main/java/ru/task/iss/util/HistoryXmlParser.java
@@ -0,0 +1,31 @@
+package ru.task.iss.util;
+/*
+ * Date: 4/24/21
+ * Time: 10:22 AM
+ * */
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.w3c.dom.Element;
+import ru.task.iss.entity.History;
+import ru.task.iss.repository.HistoryRepository;
+
+@Service
+public class HistoryXmlParser extends XmlParser {
+
+    private final HistoryRepository historyRepository;
+
+    public HistoryXmlParser(HistoryRepository historyRepository) {
+        this.historyRepository = historyRepository;
+    }
+
+    @Override
+    void initEntity(Element element) {
+
+    }
+
+    @Override
+    void saveEntity(Object entity) {
+        historyRepository.save((History) entity);
+    }
+}
