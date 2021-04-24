4201d50d703ed33c47bf4720dc791a87f414aa63
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-04-24 22:05:47 +0500

    Add createHistory test method

--- src/test/java/ru/task/iss/repository/HistoryRepositoryTest.java
+++ src/test/java/ru/task/iss/repository/HistoryRepositoryTest.java
@@ -4,7 +4,23 @@ package ru.task.iss.repository;
  * Time: 7:10 PM
  * */
 
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.task.iss.entity.History;
+
+import static org.junit.Assert.assertEquals;
+
 public class HistoryRepositoryTest extends AbstractRepositoryClass {
 
+    @Autowired
+    HistoryRepository repository;
 
+    @Test
+    public void save_ShouldSaveNewHistory() {
+        int before = repository.findAll().size();
+        History history = new History();
+        entityManager.persistAndFlush(history);
+        int after = repository.findAll().size();
+        assertEquals(before + 1, after);
+    }
 }
