dec064a2a92d8303419ee5f03f8b0b18174764a6
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-04-24 14:01:38 +0500

    Add HistoryModelAssembler

---
+++ src/main/java/ru/task/iss/controller/assembler/HistoryModelAssembler.java
@@ -0,0 +1,28 @@
+package ru.task.iss.controller.assembler;
+/*
+ * Date: 4/22/21
+ * Time: 1:02 PM
+ * */
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+import ru.task.iss.controller.HistoryController;
+import ru.task.iss.entity.History;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class HistoryModelAssembler
+        implements RepresentationModelAssembler<History, EntityModel<History>> {
+
+    @Override
+    public EntityModel<History> toModel(History entity) {
+        return EntityModel.of(entity,
+                linkTo(methodOn(HistoryController.class)
+                        .findOne(entity.getId())).withSelfRel(),
+                linkTo(methodOn(HistoryController.class)
+                        .findAll(null, null, null, null)).withRel("securities"));
+    }
+}
