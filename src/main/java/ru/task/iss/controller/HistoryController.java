bb4b256a533179cdd8e9298b205f338875843357
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-04-24 14:05:52 +0500

    Add importData method

--- src/main/java/ru/task/iss/controller/HistoryController.java
+++ src/main/java/ru/task/iss/controller/HistoryController.java
@@ -6,8 +6,16 @@ package ru.task.iss.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import ru.task.iss.controller.assembler.HistoryModelAssembler;
+import ru.task.iss.service.HistoryService;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/histories")
@@ -15,4 +23,27 @@ public class HistoryController {
 
     private static final Logger log = LoggerFactory.getLogger(HistoryController.class);
 
+    private final HistoryService historyService;
+    private final HistoryModelAssembler assembler;
+
+    public HistoryController(HistoryService historyService, HistoryModelAssembler assembler) {
+        this.historyService = historyService;
+        this.assembler = assembler;
+    }
+
+    /* Import History data from XML */
+    @PostMapping(value = "/import")
+    public ResponseEntity<String> importData(@RequestParam("file") MultipartFile file) throws IOException {
+        log.info("Starting parsing data");
+        historyService.importXmlData(file);
+        return ResponseEntity.ok("Successfully processed");
+    }
+
+    public Class<?> findOne(Long id) {
+        return null;
+    }
+
+    public Class<?> findAll(Object o, Object o1, Object o2, Object o3) {
+        return null;
+    }
 }
