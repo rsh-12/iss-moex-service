3b3a1ff01f02f748b545acb46a713d88d1431461
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-04-24 15:53:48 +0500

    Add findAll query method

--- src/main/java/ru/task/iss/repository/HistoryRepository.java
+++ src/main/java/ru/task/iss/repository/HistoryRepository.java
@@ -1,8 +1,16 @@
 package ru.task.iss.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.task.iss.entity.History;
+
+import java.time.LocalDate;
 
 public interface HistoryRepository extends JpaRepository<History, Long> {
 
+    @Query("from History h where h.tradeDate = :date")
+    Page<History> findAll(Pageable pageable, @Param("date") LocalDate tradeDate);
 }
