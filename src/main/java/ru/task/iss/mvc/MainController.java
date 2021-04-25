aafe901a860121a73c7f2f27cb75afe26b761d24
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-04-25 11:55:47 +0500

    Add mvc controller class

---
+++ src/main/java/ru/task/iss/mvc/MainController.java
@@ -0,0 +1,19 @@
+package ru.task.iss.mvc;
+/*
+ * Date: 4/25/21
+ * Time: 11:02 AM
+ * */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+    @GetMapping
+    public String home() {
+        return "index";
+    }
+}
