962b6c8aa92c72387c5c6444b7ea33b4c36dcfa7
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-04-25 17:18:36 +0500

    Add getView method

--- src/main/java/ru/task/iss/mvc/MainController.java
+++ src/main/java/ru/task/iss/mvc/MainController.java
@@ -6,14 +6,17 @@ package ru.task.iss.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class MainController {
 
-    @GetMapping
-    public String home() {
+    @GetMapping("/")
+    public String getHome() {
         return "index";
+    }
+
+    @GetMapping("/view")
+    public String getView() {
+        return "view";
     }
 }
