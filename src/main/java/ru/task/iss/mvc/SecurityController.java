669f5a1d600a6c5de8bbaee0bbcd54b90d3b3bdb
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-04-25 17:21:01 +0500

    Add getSecurity method

---
+++ src/main/java/ru/task/iss/mvc/SecurityController.java
@@ -0,0 +1,19 @@
+package ru.task.iss.mvc;
+/*
+ * Date: 4/25/21
+ * Time: 5:11 PM
+ * */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/security")
+public class SecurityController {
+
+    @GetMapping
+    public String getSecurity() {
+        return "security/security-table";
+    }
+}
