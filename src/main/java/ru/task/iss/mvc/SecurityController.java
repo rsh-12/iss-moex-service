b62b81d64ed83f9d3ffb81d77c4396f36f4a90ec
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-04-25 20:13:11 +0500

    Add getSecurities method

--- src/main/java/ru/task/iss/mvc/SecurityController.java
+++ src/main/java/ru/task/iss/mvc/SecurityController.java
@@ -5,15 +5,40 @@ package ru.task.iss.mvc;
  * */
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.task.iss.entity.Security;
+import ru.task.iss.service.SecurityService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/security")
 public class SecurityController {
 
+    private final SecurityService securityService;
+
+    public SecurityController(SecurityService securityService) {
+        this.securityService = securityService;
+    }
+
     @GetMapping
-    public String getSecurity() {
+    public String getSecurities(@RequestParam(value = "page", required = false, defaultValue = "0") Integer pageNo,
+                          @RequestParam(value = "size", required = false, defaultValue = "10") Integer pageSize,
+                          @RequestParam(value = "sort", required = false, defaultValue = "secId") String sort,
+                          Model model) {
+
+        List<Security> fields = securityService.findAllSecurities(pageNo, pageSize, sort, null);
+
+        model.addAttribute("page", pageNo);
+        model.addAttribute("size", pageSize);
+        model.addAttribute("sort", sort);
+        model.addAttribute("fields", fields);
+        model.addAttribute("reverseOrder", sort.contains("asc") ? "" : ",asc");
+
         return "security/security-table";
     }
+
 }
